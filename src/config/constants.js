a71bde24dd8c7e0389c81e7d5fc49ba8c4078695
Arsyad Sukma Ramadhan <blacklistdark1987@gmail.com>
2021-03-03 05:40:05 +0700

    Add Config & Constant

---
+++ src/config/constants.js
@@ -0,0 +1,32 @@
+export const pools = [
+  { code: "USD", name: "United States Dollars" },
+  { code: "CAD", name: "Canadian Dollar" },
+  { code: "IDR", name: "Indonesian Rupiah" },
+  { code: "GBP", name: "Pound Sterling" },
+  { code: "CHF", name: "Swiss Franc" },
+  { code: "SGD", name: "Singapore Dollar" },
+  { code: "INR", name: "Indian Rupee" },
+  { code: "MYR", name: "Malaysian Ringgit" },
+  { code: "JPY", name: "Yen" },
+  { code: "KRW", name: "Won" },
+]
+
+export const defaultPools = [
+  { code: "IDR", name: "Indonesian Rupiah" },
+  { code: "GBP", name: "Pound Sterling" },
+  { code: "CHF", name: "Swiss Franc" },
+  { code: "SGD", name: "Singapore Dollar" },
+]
+
+export const optionsDropdown = [
+  { value: "USD", value: "USD" },
+  { value: "CAD", value: "CAD" },
+  { value: "IDR", value: "IDR" },
+  { value: "GBP", value: "GBP" },
+  { value: "CHF", value: "CHF" },
+  { value: "SGD", value: "SGD" },
+  { value: "INR", value: "INR" },
+  { value: "MYR", value: "MYR" },
+  { value: "JPY", value: "JPY" },
+  { value: "KRW", value: "KRW" },
+]
