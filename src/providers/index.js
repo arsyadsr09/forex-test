97f42fba7d3a3a6b07f9b783e89ce9463921277e
Arsyad Sukma Ramadhan <blacklistdark1987@gmail.com>
2021-03-03 05:40:33 +0700

    Add Provider/API Communication

---
+++ src/providers/index.js
@@ -0,0 +1,19 @@
+import { notification } from "antd"
+import { API_URL } from "../config/config"
+
+export const getForexLatest = async () => {
+  try {
+    const ops = {
+      method: "GET",
+      url: API_URL + "/latest?base=USD",
+    }
+
+    const response = await axios(ops)
+    return response.data
+  } catch (err) {
+    notification.error({
+      message: "Server Error",
+      description: `Error fetch data from server. error: ${err}`,
+    })
+  }
+}
