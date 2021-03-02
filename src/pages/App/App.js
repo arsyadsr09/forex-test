23c02061451eb97ab5e6f15814c5bffafbf6c1a1
Arsyad Sukma Ramadhan <blacklistdark1987@gmail.com>
2021-03-03 02:40:43 +0700

    Refactor Position File

---
+++ src/pages/App/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        {/* static USD */}
+        <div className="row">
+          <div className="col-12">
+            <span>lalala</span>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
