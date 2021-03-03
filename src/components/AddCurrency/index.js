1875faf5380a1bb7b1940210dc9cd9bec1d95fd9
Arsyad Sukma Ramadhan <blacklistdark1987@gmail.com>
2021-03-03 09:10:37 +0700

    Add Component AddCurrency

---
+++ src/components/AddCurrency/index.js
@@ -0,0 +1,42 @@
+import React, { useContext } from "react"
+import { Col, Button, AutoComplete, Input } from "antd"
+import RootContext from "../../context"
+
+function AddCurrency() {
+  const {
+    onSelectCurrency,
+    selectedCurrency,
+    addCurrencyStatus,
+    onSubmitCurrency,
+    setAddCurrencyStatus,
+    optionsCurrency,
+  } = useContext(RootContext)
+
+  return (
+    <Col span={24} style={{ marginTop: "1rem" }}>
+      {!addCurrencyStatus ? (
+        <Button
+          type="primary"
+          block
+          onClick={() => setAddCurrencyStatus(!addCurrencyStatus)}
+        >
+          (+) Add Currency
+        </Button>
+      ) : (
+        <Col className="d-flex justify-content-center">
+          <AutoComplete
+            options={optionsCurrency}
+            value={selectedCurrency}
+            style={{ width: "75%" }}
+            onSelect={onSelectCurrency}
+          />
+          <Button type="primary" onClick={onSubmitCurrency}>
+            Submit
+          </Button>
+        </Col>
+      )}
+    </Col>
+  )
+}
+
+export default AddCurrency
