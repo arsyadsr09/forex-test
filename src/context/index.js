69c41746d25f2332f544ea10e6097bb944e8b873
Arsyad Sukma Ramadhan <blacklistdark1987@gmail.com>
2021-03-03 09:10:10 +0700

    Add context

--- src/context/index.js
+++ src/context/index.js
@@ -1,36 +1,83 @@
 import { createContext, useState, useEffect } from "react"
-import { defaultPools } from "../config/constants"
+import { defaultPools, pools, optionsDropdown } from "../config/constants"
 import { getForexLatest } from "../providers"
 
 export const RootContext = createContext()
 
-export function RootContextProvider() {
+export function RootContextProvider(props) {
   const [amountBaseCurrency, setAmountBaseCurrency] = useState(1)
   const [usingCurrency, setUsingCurrency] = useState(defaultPools)
-  const [listCurrencyFromAPI, setListCurrencyFromAPI] = useState()
+  const [optionsCurrency, setOptionsCurrency] = useState(optionsDropdown)
+  const [listCurrency, setListCurrency] = useState(pools)
   const [selectedCurrency, setSelectedCurrency] = useState("")
+  const [addCurrencyStatus, setAddCurrencyStatus] = useState(false)
 
   const onSelectCurrency = (value) => {
     console.log(value)
 
     setSelectedCurrency(value)
+    filterWithoutUsingCurrency()
+  }
+
+  const onSubmitCurrency = () => {
+    let _temp = listCurrency.filter((item) => selectedCurrency === item.code)
+    setUsingCurrency([...usingCurrency, _temp[0]])
+    setSelectedCurrency("")
+    setAddCurrencyStatus(false)
+    // filterWithoutUsingCurrency
   }
 
   const onChangeAmountBaseCurrency = (value) => {
     setAmountBaseCurrency(value)
   }
 
+  const getPriceByCodeCurrency = (code) => {
+    return listCurrency.filter(
+      (item) => item.code.toUpperCase() === code.toUpperCase()
+    )[0].price
+  }
+
+  const onRemoveFromUsingCurrency = (code) => {
+    let _tempUsingCurrency = usingCurrency.filter(
+      (i) => i.code.toUpperCase() !== code.toUpperCase()
+    )
+
+    setUsingCurrency(_tempUsingCurrency)
+  }
+
   const getLatest = async () => {
     try {
-      const forexLatest = await getForexLatest()
-      setListCurrencyFromAPI(forexLatest)
+      const _forexLatest = await getForexLatest()
+      let _temp = listCurrency.map((item) => {
+        for (const property in _forexLatest.rates) {
+          if (property.toUpperCase() === item.code.toUpperCase()) {
+            return {
+              ...item,
+              price: _forexLatest.rates[property],
+            }
+          }
+        }
+      })
+      setListCurrency(_temp)
     } catch (err) {
       console.error(err)
     }
   }
 
+  const filterWithoutUsingCurrency = () => {
+    let _tempOptions = []
+    optionsCurrency.forEach((e) => {
+      if (!JSON.stringify(usingCurrency).includes(e.value)) {
+        _tempOptions.push({ value: e.value, label: e.value, name: e.name })
+      }
+    })
+
+    setOptionsCurrency(_tempOptions)
+  }
+
   useEffect(() => {
     getLatest()
+    filterWithoutUsingCurrency()
   }, [])
 
   return (
@@ -39,13 +86,23 @@ export function RootContextProvider() {
         amountBaseCurrency,
         usingCurrency,
         selectedCurrency,
+        addCurrencyStatus,
+        optionsCurrency,
+        setAddCurrencyStatus,
         onSelectCurrency,
         onChangeAmountBaseCurrency,
+        onSubmitCurrency,
+        onRemoveFromUsingCurrency,
+        getPriceByCodeCurrency,
       }}
     >
-      {this.props.children}
+      {props.children}
     </RootContext.Provider>
   )
+}
+
+export function RootContextConsumer({ children }) {
+  return <RootContext.Consumer>{children}</RootContext.Consumer>
 }
 
 export default RootContext
